7a46e24f4049c6ef90730a71524ffbee6cf62b55
Cluster <clusterrr@clusterrr.com>
2015-05-26 05:34:36 +0300

    timings

--- main.c
+++ main.c
@@ -72,7 +72,7 @@ int main()
 
 		if (!connected)
 		{
-			_delay_ms(1000);
+			_delay_ms(500);
 			connected = 1;
 			WII_CONNECT;
 			wm_init((void*)classic_controller_id, but_dat, (void*)cal_data, wiimote_query);
